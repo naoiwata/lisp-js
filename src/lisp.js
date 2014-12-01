0f308ad3d636560a6ed1a98a448631b03f99060c
naoiwata <orrrizzle@gmail.com>
2014-12-01 23:00:57 +0900

    Fix syntax Error

--- src/lisp.js
+++ src/lisp.js
@@ -399,29 +399,6 @@ var CondToIf = function(exp) {
   return expandClauses(condClauses(exp));
 };
 
-var expandClauses = function(clauses) {
-  if (isNull(clauses)) {
-    return 'false';
-  }
-  
-}
-
-(define (expand-clauses clauses)
-  (if (null? clauses)
-      'false                          ; else節なし
-      (let ((first (car clauses))
-            (rest (cdr clauses)))
-        (if (cond-else-clause? first)
-            (if (null? rest)
-                (sequence->exp (cond-actions first))
-                (error "ELSE clause isn't last -- COND->IF"
-                       clauses))
-            (make-if (cond-predicate first)
-                     (sequence->exp (cond-actions first))
-                     (expand-clauses rest))))))'
-
-
-
 /* --------------------------
  * Utilities
  * -------------------------- */
