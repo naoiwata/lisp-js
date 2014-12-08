703f3abef1bbb886b05b52ea80bbceaca3a288a3
naoiwata <orrrizzle@gmail.com>
2014-12-09 05:52:18 +0900

    setup procedures

--- src/setup.js
+++ src/setup.js
@@ -35,3 +35,51 @@ var priitiveProcesureObjects = function() {
   return Map(primitiveProcedures,
              function(proc) {return List('primitive', Car(proc))});
 };
+
+var applyPrimitiveProcedure = function(proc, args) {
+  return applyInUnderlyingScheme(primitiveImplementation(proc),
+                                 args);
+};
+
+var inputPrompt = ';;; M-Eval input: ';
+
+var outputPrompt = ';;; M-Eval value: ';
+
+var driverLoop = function() {
+  promptForInput(inputPrompt);
+  // TODO: read()
+  var input = read();
+  var output = Eval(input, theGlobalEnvironment);
+  announceOutput(outputPrompt);
+  userPrint(output);
+  return driverLoop();
+};
+
+var promptForInput = function(string) {
+  // TODO: newline(), display()
+  newline();
+  newline();
+  display(string);
+  newline();
+};
+
+var announceOutput = function(string) {
+  newline();
+  display(string);
+  newline();
+};
+
+var userPrint = function(object) {
+  if (isCompoundProcedure(object)) {
+    display(List('compondProceure',
+                 procedureParameters(object),
+                 procedureBody(object)));
+  }
+  else {
+    display(object);
+  }
+};
+
+var theGlobalEnvironment = setupEnvironment();
+
+deriverLoop();
