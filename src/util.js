f8086539c6bd114609e827399ff7152fbb00ad55
naoiwata <orrrizzle@gmail.com>
2014-12-10 18:48:28 +0900

    Set module util.js

--- src/util.js
+++ src/util.js
@@ -1,26 +1,46 @@
 /* jshint globalstrict:true */
 'use strict';
 
-var _ = require('underscore');
+var _ = require('underscore'),
+    base = require('./base');
 
+var False = base.False,
+    Car = base.Car,
+    isEq = base.isEq,
+    isPair = base.isPair;
 
-var NumberToString = function(v) {
-  if (!_.isNumber(v)) {
-    throw new TypeError(v);
-  }
-  return String(v);
-};
+module.exports = (function() {
 
-var StringToNumber = function(v) {
-  if (!_.isString(v) || v === '' || _.isNaN(Number(v))) {
-    throw new TypeError(v);
-  }
-  return Number(v);
-};
+  var NumberToString = function(v) {
+    if (_.isNumber(v)) {
+      return String(v);
+    }
+    else {
+      throw new TypeError(v);
+    }
+  };
 
-var isTaggedList = function(exp, tag) {
-  if (isPair(exp)) {
-    return isEq(Car(exp), tag);
-  }
-  return False;
-};
+  var StringToNumber = function(v) {
+    if (_.isString(v) && v !== '' && !_.isNaN(Number(v))) {
+      return Number(v);
+    }
+    else {
+      return Number(v);
+    }
+  };
+
+  var isTaggedList = function(exp, tag) {
+    if (isPair(exp)) {
+      return isEq(Car(exp), tag);
+    }
+    return False;
+  };
+
+  return {
+    NumberToString: NumberToString,
+    StringToNumber: StringToNumber,
+    isTaggedList: isTaggedList
+  };
+
+}());
+
