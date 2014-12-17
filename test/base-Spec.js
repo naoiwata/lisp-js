179c25b01b76f2769ae1f24271e6dfc791d42765
naoiwata <orrrizzle@gmail.com>
2014-12-17 14:18:29 +0900

    Add properties on base.js UnitTest

--- test/base-Spec.js
+++ test/base-Spec.js
@@ -3,4 +3,22 @@
 
 describe('base', function() {
   var base = require('base');
+
+  describe('Nil', function() {
+    it('Nil to be null in JavaScript', function() {
+      expect(base.Nil).to.be.eql(null);
+    });
+  });
+
+  describe('True', function() {
+    it('True to be true in JavaScript', function() {
+      expect(base.True).to.be.eql(true);
+    });
+  });
+
+  describe('False', function() {
+    it('False to be false in JavaScript', function() {
+      expect(base.False).to.be.eql(false);
+    });
+  });
 });
