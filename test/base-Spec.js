4c514ba854795361a5f196df98bdbd0310026474
naoiwata <orrrizzle@gmail.com>
2014-12-18 22:37:43 +0900

    Refactor

--- test/base-Spec.js
+++ test/base-Spec.js
@@ -24,115 +24,119 @@ describe('base', function() {
 
   describe('And', function() {
     var True = base.True,
-        False = base.False;
+        False = base.False,
+        And= base.And;
     it('And(True, True, True) should return true', function() {
-      expect(base.And(True, True, True)).to.be.true;
+      expect(And(True, True, True)).to.be.true;
     });
 
     it('And(True, True, False) should return false', function() {
-     expect(base.And(True, True, False)).to.be.false;
+     expect(And(True, True, False)).to.be.false;
     });
 
     it('And(True, False, True) should return false', function() {
-      expect(base.And(True, False, True)).to.be.false;
+      expect(And(True, False, True)).to.be.false;
     });
 
     it('And(False, True, True) should return false', function() {
-      expect(base.And(False, True, True)).to.be.false;
+      expect(And(False, True, True)).to.be.false;
     });
 
     it('And(False, False, False) should return false', function() {
-      expect(base.And(False, False, False)).to.be.false;
+      expect(And(False, False, False)).to.be.false;
     });
   });
 
   describe('Or', function() {
     var True = base.True,
-        False = base.False;
+        False = base.False,
+        Or = base.Or;
     it('Or(True, True, True) should return true', function() {
-      expect(base.Or(True, True, True)).to.be.true;
+      expect(Or(True, True, True)).to.be.true;
     });
 
     it('Or(True, True, False) should return true', function() {
-     expect(base.Or(True, True, False)).to.be.true;
+     expect(Or(True, True, False)).to.be.true;
     });
 
     it('Or(True, False, True) should return true', function() {
-      expect(base.Or(True, False, True)).to.be.true;
+      expect(Or(True, False, True)).to.be.true;
     });
 
     it('Or(False, True, True) should return true', function() {
-      expect(base.Or(False, True, True)).to.be.true;
+      expect(Or(False, True, True)).to.be.true;
     });
 
     it('Or(False, False, False) should return false', function() {
-      expect(base.Or(False, False, False)).to.be.false;
+      expect(Or(False, False, False)).to.be.false;
     });
   });
 
   describe('isEq', function() {
+    var isEq = base.isEq;
     it('isEq(<Number>, <Number>) return true', function() {
-      expect(base.isEq(100, 100)).to.be.true;
+      expect(isEq(100, 100)).to.be.true;
     });
 
     it('isEq(<Number>, <Not Number>) return false', function() {
-      expect(base.isEq(100, '100')).to.be.false;
+      expect(isEq(100, '100')).to.be.false;
     });
 
     it('isEq(<String>, <String>) return true', function() {
-      expect(base.isEq('hoge', 'hoge')).to.be.true;
+      expect(isEq('hoge', 'hoge')).to.be.true;
     });
 
     it('isEq(<Function>, <Function>) return true', function() {
       var func = function() {};
-      expect(base.isEq(func, func)).to.be.true;
+      expect(isEq(func, func)).to.be.true;
     });
 
     it('isEq(<Function1>, <Function2>) return false', function() {
-      expect(base.isEq(function() {}, function() {})).to.be.false;
+      expect(isEq(function() {}, function() {})).to.be.false;
     });
 
     it('isEq(<Object>, <Object>) return true', function() {
       var obj = {};
-      expect(base.isEq(obj, obj)).to.be.true;
+      expect(isEq(obj, obj)).to.be.true;
     });
 
     it('isEq(<Object1>, <Object2>) return false', function() {
       var obj = {};
-      expect(base.isEq({}, {})).to.be.false;
+      expect(isEq({}, {})).to.be.false;
     });
   });
 
   describe('isEqual', function() {
+    var isEqual = base.isEqual;
     it('isEqual(<Number>, <Number>) return true', function() {
-      expect(base.isEqual(100, 100)).to.be.true;
+      expect(isEqual(100, 100)).to.be.true;
     });
 
     it('isEqual(<Number>, <Not Number>) return false', function() {
-      expect(base.isEqual(100, '100')).to.be.false;
+      expect(isEqual(100, '100')).to.be.false;
     });
 
     it('isEqual(<String>, <String>) return true', function() {
-      expect(base.isEqual('hoge', 'hoge')).to.be.true;
+      expect(isEqual('hoge', 'hoge')).to.be.true;
     });
 
     it('isEqual(<Function>, <Function>) return true', function() {
       var func = function() {};
-      expect(base.isEqual(func, func)).to.be.true;
+      expect(isEqual(func, func)).to.be.true;
     });
 
     it('isEqual(<Function1>, <Function2>) return false', function() {
-      expect(base.isEqual(function() {}, function() {})).to.be.false;
+      expect(isEqual(function() {}, function() {})).to.be.false;
     });
 
     it('isEqual(<Object>, <Object>) return true', function() {
       var obj = {};
-      expect(base.isEqual(obj, obj)).to.be.true;
+      expect(isEqual(obj, obj)).to.be.true;
     });
 
     it('isEqual(<Object1>, <Object2>) return true', function() {
       var obj = {};
-      expect(base.isEqual({}, {})).to.be.true;
+      expect( isEqual({}, {})).to.be.true;
     });;
   });
 });
